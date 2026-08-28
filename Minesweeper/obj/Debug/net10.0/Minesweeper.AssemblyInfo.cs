7da42693b5e8a49c53ccb071e76f0d2c80b1710b
Jessen1799 <jessen170499@gmail.com>
2026-08-28 16:53:02 +0200

    Small change to UI

--- Minesweeper/obj/Debug/net10.0/Minesweeper.AssemblyInfo.cs
+++ Minesweeper/obj/Debug/net10.0/Minesweeper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+587dd2bdc2649a29cefb33160327a9fb9fddee4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32bd11d87a729b5ff1acbc8fe34e16409b25f364")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
